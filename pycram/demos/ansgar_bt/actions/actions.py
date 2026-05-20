b298e5785411f6c686a521afdbab4064a260635e
endxrpxl <ansgar.joerss@t-online>
2026-05-20 01:25:37 +0000

    add FreeSurfaceAction

---
+++ pycram/demos/ansgar_bt/actions/actions.py
@@ -0,0 +1,51 @@
+from dataclasses import dataclass, field
+from typing import Optional
+
+from demos.ansgar_bt.helpers.object_helpers import placement_poses_on_surface
+from pycram.datastructures.enums import Arms
+from pycram.plans.factories import execute_single
+from pycram.robot_plans.actions.base import ActionDescription
+from pycram.robot_plans.actions.composite.transporting import TransportAction
+from semantic_digital_twin.reasoning.queries import (
+    preferred_surface_for_object,
+    get_next_object_using_planar_distance,
+)
+from semantic_digital_twin.semantic_annotations.mixins import HasSupportingSurface
+from semantic_digital_twin.spatial_types import Vector3
+
+
+@dataclass
+class FreeSurfaceAction(ActionDescription):
+    """
+    Transports an object to a position on the surface using an arm
+    """
+
+    surface_to_clean: HasSupportingSurface = field(repr=False)
+    """
+    Semantic annotation describing the object that should be transported.
+    """
+
+    arm: Optional[Arms]
+    """
+    Arm that should be used
+    """
+
+    def execute(self) -> None:
+        objects_on_surface = get_next_object_using_planar_distance(
+            main_body=self.robot.root,
+            supporting_surface=self.surface_to_clean,
+            ignore_dimension=Vector3(z=1.0),
+        ).tolist()
+        for obj in objects_on_surface:
+            target_surfaces = preferred_surface_for_object(obj).tolist()
+            place_poses = placement_poses_on_surface(
+                surface=target_surfaces[0],
+                obj=obj,
+            )
+
+            transport_action = TransportAction(
+                semantic_annotation=obj,
+                target_location=place_poses[0],
+                arm=self.arm,
+            )
+            self.add_subplan(execute_single(transport_action)).perform()
